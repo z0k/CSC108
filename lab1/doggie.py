424bd63e57620a0dd460ba2faa78b609bb351b43
Mahmud Zarak <c3mahmud@cdf.toronto.edu>
2013-01-25 02:29:43 -0500

    Updated with lab file.

---
+++ lab1/doggie.py
@@ -0,0 +1,29 @@
+age = 18
+years_in_school = age - 5
+
+print("Did you know you've been in school for about")
+print(years_in_school)
+print("years")
+
+retirement_age = 65
+years_until_retirement = retirement_age - age
+
+print("and you have")
+print(years_until_retirement)
+print("years until you can retire?")
+
+# applying the rule that one human year is seven doggie years
+dog_years = age * 7
+print("If you are a hound, you are")
+print(dog_years)
+print("doggie-years old.")
+
+# let's make you 10 years older for fun
+age = age + 10
+
+print("You've aged and are now")
+print(age)
+dog_years = age * 7
+# But see that dog_years hasn't changed
+print("And your doggie age is now")
+print(dog_years)
