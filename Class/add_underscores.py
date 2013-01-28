58b04d06f866e7f452b5ecfb2daca7c819816bd7
Zarak Mahmud <zarak.mahmud@gmail.com>
2013-01-28 16:11:54 -0500

    add_underscores method complete.

--- Class/add_underscores.py
+++ Class/add_underscores.py
@@ -1,2 +1,8 @@
+#def add_underscores(s):
+    #return '_'.join(s)
+
 def add_underscores(s):
-    return '_'.join(s)
+    underscored_string = ''
+    for e in s:
+        underscored_string = underscored_string + e + '_'
+    return underscored_string[:-1]
