351e203e490c4698bc9f7da646ae58752c2dff30
Zarak Mahmud <zarak.mahmud@gmail.com>
2013-02-04 22:09:05 -0500

    Worked up to half_revealed function.

--- Assignments/puzzler_functions.py
+++ Assignments/puzzler_functions.py
@@ -134,3 +134,118 @@ def make_guessed(unguessed_consonants, unguessed_vowels, letter):
         return (unguessed_consonants, unguessed_vowels.replace(letter, ''))
     else:
         return (unguessed_consonants, unguessed_vowels)
+
+
+def calculate_score(current_score, num_occurrences, letter_type):
+    """(int, int, str) -> int
+    
+    Return the new score by adding one point per num_occurrences of the letter
+    to current_score if letter_type is CONSONANT, or by deducting VOWEL_PRICE
+    from current_score if letter_type is VOWEL.
+
+
+    >>> calculate_score(0, 5, 'C')
+    5
+    >>> calculate_score(5, 10, 'C')
+    15
+    >>> calculate_score(5, 2, 'V')
+    4
+    """
+    if letter_type == CONSONANT:
+        return current_score + num_occurrences
+    elif letter_type == VOWEL:
+        return current_score - VOWEL_PRICE
+
+
+#I feel a bit iffy about this function, make sure to check it thoroughly.
+def finalize_score(puzzle, view, unguessed_consonants, current_score):
+    """(str, str, str, int) -> int
+    
+    Return the final score, which is calculated by adding CONSONANT_BONUS to
+    current_score for each unguessed consonants from the puzzle that is still
+    hidden in the view. 
+    
+
+    >>> finalize_score('hello there', 'he^^o ^^e^e', 'bcdlthr', 0)
+    8
+    >>> finalize_score('which ones count', '^^^^^ ^^^^ ^^^^^', CONSONANTS, 2)
+    12
+    """
+    j  = 0
+    for i in range(len(puzzle)):
+        if (not puzzle[i] == view[i]) and (puzzle[i] in unguessed_consonants):
+            guess = make_guessed(unguessed_consonants, '', puzzle[i])
+            unguessed_consonants = guess[0]
+            j += 1
+    return j * CONSONANT_BONUS + current_score
+    
+
+def update_score(playerone_score, playertwo_score, new_score, current_player):
+    """(int, int, int, str) -> (int, int)
+    
+    Return an updated playerone_score or playertwo_score, depending on who is 
+    the current_player, as the tuple (new_score, current_player)
+    
+    
+    >>> update_score(10, 0, ???)
+    """
+    pass
+                     
+
+def next_player(current_player, num_occurrences):
+    """(str, int) -> str
+                     
+    Return the next player, depending on who the current_player is, and if they
+    guessed num_occurrences to be greater than zero.
+
+
+    >>> next_player(PLAYER_ONE, 0)
+    'Player Two'
+    >>> next_player(PLAYER_ONE, 3)
+    'Player One'
+    """
+    if current_player == PLAYER_ONE:
+        if num_occurrences == 0:
+            return PLAYER_TWO
+        return PLAYER_ONE
+    elif current_player == PLAYER_TWO:
+        if num_occurrences == 0:
+            return PLAYER_ONE
+        return PLAYER_TWO
+
+
+def guess_letter(unguessed_consonants, difficulty):
+    """(str, str) -> str
+    
+    Return the consonant to be guessed next by the computer player. If 
+    the difficulty is EASY, the guess is randomly selected. If difficulty is
+    HARD, the consonant to guess is the first consonant in PRIORITY_CONSONANTS
+    that occurs in unguessed_consonants.
+
+
+    >>> guess_letter('tfgh', HARD)
+    't'
+    >>> guess_letter('fgh', HARD)
+    'h'
+    """
+    if difficulty == EASY:
+        return random.choice(unguessed_consonants)
+    elif difficulty == HARD:
+        for e in PRIORITY_CONSONANTS:
+            if e in unguessed_consonants:
+                return e
+
+    
+def half_revealed(view):
+    """(str) -> bool
+    
+    Return True iff at least half of the alphabetic characters in the view are
+    revealed.
+    
+    
+    >>> half_revealed('he^^o ^^e^e')
+    True
+    >>> half_revealed('h^^^^ ^^^^^')
+    False
+    """
+    return  view.count(HIDDEN) <= get_view(view).count(HIDDEN) / 2.
