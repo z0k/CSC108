a63e3ad11be0ba5c0775b1754345d8d3170096b9
Zarak Mahmud <zarak.mahmud@gmail.com>
2013-02-11 17:00:29 -0500

    Class functions done.

--- Class/digital_sum.py
+++ Class/digital_sum.py
@@ -1,4 +1,6 @@
 def can_pay_with_two_coins(denoms, amount):
+    if amount == 0:
+        return False
     for i in range(len(denoms)):
         for j in range(len(denoms)):
             if (denoms[i] + denoms[j]) % amount == 0:
